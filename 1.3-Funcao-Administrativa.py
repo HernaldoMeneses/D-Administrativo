c0addc8d7dd5df0e6d463be47d80b2a875983bb8
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-03-11 10:39:25 -0300

    Create 1.3-funcao-Administrativa.py

---
+++ 1.3-Funcao-Administrativa.py
@@ -0,0 +1,19 @@
+point_doutrina = '''
+    OTTO MAYER
+        A administrativa é a atividade do Estado para realizar seus fins,
+        debaixo da ordem jurídica".
+
+            Sujeito (aspectio subjetivo)        - (Quem?)
+            efeitos (aspectios objetivo formal) - (O quê?)
+
+            Identificação
+                1 - Subjetivo (ou orgânico) - realce ao agente da função (quem?)
+                2 - objetivo maretial - realce ao conteúdo da atividade
+                3 - objetivo forma - realce o fegime jurídico em que se situa
+            
+            ** não isolar os cirtérios, são coexistentes
+'''
+
+#Reler
+#75/2208
+#file:///L:/Linear_/D-Administrativo/Jos%C3%A9%20dos%20Santos%20Carvalho%20Filho%20-%20Manual%20de%20Direito%20Administrativo%20-%2034%20ed.%20(2020).pdf
